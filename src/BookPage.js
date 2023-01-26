1ce75a620898aa69add39e9af5f169d289e91a5c
ameerhamzakhalid <ameerhamza.khalid1997@gmail.com>
2023-01-26 14:17:21 +0500

    made  Api integration
     I have integrated API
    I have used Axios

--- src/BookPage.js
+++ src/BookPage.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import Nav from './components/Nav';
-import ActionNameButton from './components/BookComponent/ActionNameButton';
 import Percentage from './components/BookComponent/Percentage';
 import style from './BookPage.module.css';
+import EachBook from './components/BookComponent/eachBook';
 import AddBook from './components/BookComponent/AddBook';
+import { booksFromAPI } from './redux/books/books';
 
 function BookPage() {
+  const books = useSelector((state) => state.Books, shallowEqual);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(booksFromAPI());
+  }, []);
   return (
     <>
       <Nav />
       <div className={style.body}>
         <div className={style.action}>
-          <ActionNameButton
-            userName="Ameer Hamza"
-            gameName="Sokudo"
-            category="Moon"
-          />
+          {books.map((book) => (
+            <EachBook
+              topic={book.topic}
+              author={book.author}
+              title={book.title}
+              key={book.item_id}
+              id={book.item_id}
+              category={book.category}
+            />
+          ))}
+
           <Percentage />
         </div>
       </div>
